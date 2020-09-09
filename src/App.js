7b0d4a461c75eabcc54114ca39d3b156c023c0bd
aryan007007 <aryanasthana211@gmail.com>
2020-09-09 18:31:52 +0530

    1. Configured index.js file for rendering app.js 2. Created components for the input section of the application

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TextInput from './TextInput'
+import './style.css'
+
+function App() {
+    return (
+
+        <div className = "centeredForm">
+            <TextInput />
+        </div>
+
+    );
+}
+
+export default App;
+
+
+
+   
